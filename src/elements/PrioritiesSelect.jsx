5a7b18af7a950a51ff553757e46e210847cad3c8
elbajo001 <elbajo002@hotmail.com>
2023-10-14 23:52:25 -0500

    to do taks v1.2

--- src/elements/PrioritiesSelect.jsx
+++ src/elements/PrioritiesSelect.jsx
@@ -9,7 +9,7 @@ const PrioritiesSelect = (props) => {
         props?.handleChangePriority(props?.dataPriority);
       }}
       className={`${ props?.priority === props?.dataPriority?.id
-                ? `bg-${props?.dataPriority?.color}-50  ring-${props?.dataPriority?.color}-300 text-slate-800 `
+                ? `bg-${props?.dataPriority?.color}-50 ring-${props?.dataPriority?.color}-300 text-slate-800 `
                 : `bg-gray-100 ring-slate-300 text-slate-500 font-regular hover:bg-${props?.dataPriority?.color}-100 hover:ring-${props?.dataPriority?.color}-300 hover:text-slate-800 `
             } 
             container_priorities-select rounded-md py-2 px-8 ring-1 cursor-pointer transition-all w-1/3 md:w-fit md:px-16 text-center
