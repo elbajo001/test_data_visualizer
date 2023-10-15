2412fec5a850e1d1bb6a6ae4ae80f7b8a0afffe4
elbajo001 <elbajo002@hotmail.com>
2023-10-14 23:55:20 -0500

    to do taks v1.2

--- src/elements/PrioritiesSelect.jsx
+++ src/elements/PrioritiesSelect.jsx
@@ -8,12 +8,7 @@ const PrioritiesSelect = (props) => {
       onClick={() => {
         props?.handleChangePriority(props?.dataPriority);
       }}
-      className={`${ props?.priority === props?.dataPriority?.id
-                ? `bg-${props?.dataPriority?.color}-50 ring-${props?.dataPriority?.color}-300 text-slate-800 `
-                : `bg-gray-100 ring-slate-300 text-slate-500 font-regular hover:bg-${props?.dataPriority?.color}-100 hover:ring-${props?.dataPriority?.color}-300 hover:text-slate-800 `
-            } 
-            container_priorities-select rounded-md py-2 px-8 ring-1 cursor-pointer transition-all w-1/3 md:w-fit md:px-16 text-center
-      `}>
+      className={`bg-gray-100 ring-slate-300 text-slate-500 font-regular hover:bg-orange-100 hover:ring-orange-300 hover:text-slate-800  container_priorities-select rounded-md py-2 px-8 ring-1 cursor-pointer transition-all w-1/3 md:w-fit md:px-16 text-center`}>
       {props?.dataPriority?.label}
     </button>
   );
